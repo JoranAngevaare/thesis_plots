bbc4d069cce75c0ce60275188c776e94611fd567
Joran Angevaare <j.angevaare@nikhef.nl>
2022-10-07 15:46:45 +0200

    fix tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,9 +1,10 @@
 """Basic tests and imports"""
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 import thesis_plots
-import matplolib.pyplot as plt
+
 
 def test_print_versions():
     thesis_plots.print_versions()
@@ -25,8 +26,10 @@ def test_to_str_tuple():
         if len(res):
             assert isinstance(res[0], str), res
 
+
 def test_limit_setter():
     ob = thesis_plots.LimitSetter()
+
 
 def test_axhline():
     thesis_plots.labeled_hline()
