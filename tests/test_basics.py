0f82678722cd8817d5519ed4bdf49b78363be936
Joran Angevaare <j.angevaare@nikhef.nl>
2022-10-07 16:19:21 +0200

    100%

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,25 +1,32 @@
 """Basic tests and imports"""
 import matplotlib.pyplot as plt
 import thesis_plots
-
+import numpy as np
+from unittest import TestCase
 
 def test_print_versions():
     thesis_plots.print_versions()
     thesis_plots.print_versions(['something_not_installed'])
 
 
-def test_to_str_tuple():
-    tests = [
-        'a',
-        tuple(),
-        ['a', 'b'],
-        ('a', 'b'),
-    ]
-    for t in tests:
-        res = thesis_plots.to_str_tuple(t)
-        assert isinstance(res, tuple)
-        if len(res):
-            assert isinstance(res[0], str), res
+class TestToStrTuple(TestCase):
+    def test_to_str_tuple(self):
+        tests = [
+            'a',
+            tuple(),
+            ['a', 'b'],
+            ('a', 'b'),
+        ]
+        for t in tests:
+            res = thesis_plots.to_str_tuple(t)
+            assert isinstance(res, tuple)
+            if len(res):
+                assert isinstance(res[0], str), res
+
+    def test_bad_imput(self):
+        with self.assertRaises(TypeError):
+            thesis_plots.to_str_tuple(np.array(['a']))
+
 
 
 def test_limit_setter():
