968957acb0ec3b13cb7d859099aed734eb35d69a
Joran Angevaare <j.angevaare@nikhef.nl>
2022-10-07 15:41:47 +0200

    fix coverage

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 import thesis_plots
-
+import matplolib.pyplot as plt
 
 def test_print_versions():
     thesis_plots.print_versions()
@@ -23,3 +23,11 @@ def test_to_str_tuple():
         assert isinstance(res, tuple)
         if len(res):
             assert isinstance(res[0], str), res
+
+def test_limit_setter():
+    ob = thesis_plots.LimitSetter()
+
+def test_axhline():
+    thesis_plots.labeled_hline()
+    thesis_plots.labeled_vline()
+    plt.clf()
