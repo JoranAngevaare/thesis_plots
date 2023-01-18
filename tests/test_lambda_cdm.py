54e1eb3f7a006f89bf2a4ddc1064b60568345dab
Joran Angevaare <j.angevaare@nikhef.nl>
2023-01-18 15:02:04 +0100

    also recoil rates

--- tests/test_lambda_cdm.py
+++ tests/test_lambda_cdm.py
@@ -3,5 +3,5 @@ import thesis_plots
 
 def test_make_rebin_false():
     thesis_plots.setup_plt()
-    thesis_plots.PlotLambdaCDM().plot(make_rebin=False)
+    thesis_plots.PlotLambdaCDM(make_rebin=False).plot()
     thesis_plots.save_fig('planck_cdm')
